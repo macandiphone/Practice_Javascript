bd846ebf153061e4b77bf645e428c7a3f64755f2
Alex <luckyone@tutamail.com>
2022-02-24 16:22:59 -0500

    Button Ripple Effect

---
+++ index.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.btn');
+btn.addEventListener('mouseover', (e) => {
+  // console.log(e.pageX - btn.offsetLeft); -1 , 125
+  // console.log(e.pageY - btn.offsetTop); -1 , 57
+
+  const x = e.pageX - btn.offsetLeft;
+  const y = e.pageY - btn.offsetTop;
+
+  btn.style.setProperty('--xPos', x + 'px');
+  btn.style.setProperty('--yPos', y + 'px');
+});
