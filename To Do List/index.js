082eb8ca78541ccf4b667cc671601cd9378b3b31
Alex <luckyone@tutamail.com>
2022-03-07 02:33:07 -0500

    to do list

---
+++ To Do List/index.js
@@ -0,0 +1,63 @@
+const formEl = document.querySelector(".form");
+const inputEl = document.querySelector(".input");
+const ulEL = document.querySelector(".list");
+let list = JSON.parse(localStorage.getItem("list"));
+
+list.forEach((task) => {
+  toDoList(task);
+});
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  toDoList();
+});
+
+function toDoList(task) {
+  let newTask = inputEl.value;
+  if (task) {
+    newTask = task.name;
+  }
+
+  const liEl = document.createElement("li");
+  if (task && task.checked) {
+    liEl.classList.add("checked");
+  }
+  liEl.innerText = newTask;
+  ulEL.appendChild(liEl);
+  inputEl.value = "";
+
+  const checkBtnEl = document.createElement("div");
+  checkBtnEl.innerHTML = `
+  <i class="fa-solid fa-square-check">
+  `;
+  liEl.appendChild(checkBtnEl);
+
+  const trashBtnEl = document.createElement("div");
+  trashBtnEl.innerHTML = `
+  <i class="fa-solid fa-trash">
+  `;
+  liEl.appendChild(trashBtnEl);
+
+  checkBtnEl.addEventListener("click", () => {
+    liEl.classList.toggle("checked");
+    updateLocalStorage();
+  });
+
+  trashBtnEl.addEventListener("click", () => {
+    liEl.remove();
+    updateLocalStorage();
+  });
+  updateLocalStorage();
+}
+
+function updateLocalStorage() {
+  const liEls = document.querySelectorAll("li");
+  list = [];
+  liEls.forEach((li) => {
+    list.push({
+      name: li.innerText,
+      checked: li.classList.contains("checked"),
+    });
+  });
+  localStorage.setItem("list", JSON.stringify(list));
+}
